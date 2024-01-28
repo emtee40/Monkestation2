HolyTigerGod/Voidcrew

SPECIES NUKING 2023: Mein leber! Allows livers to handle reagents in special ways, instead of the species datum doing it (#76184)
## About The Pull Request

Refactors livers so special chemical handling can be done by them,
instead of the species datum.
Plasmamen, skeletons and golems all use the liver for all their species
specific chem handling now.

## Why It's Good For The Game

SPECIES DATUM I HATE YOU!
Also, being able to handle reagents like any species if you have their
liver is REALLY FREAKING COOL and allows for emergent gameplay by mixing
various organs from various sources.

## Changelog

:cl:
refactor: Mutant livers can now handle chemicals in special ways.
Currently, only plasmaman, skeleton and golem livers do it. Every other
species is the same.
/:cl:

---------

Co-authored-by: Time-Green <7501474+Time-Green@users.noreply.github.com>

code/modules/surgery/organs/internal/liver/liver_plasmaman.dm
@@ -1,0 +1,25 @@
+/**
+ * Plasmaman liver
+ * Makes plasma and hot ice heal wounds, also makes gunpowder a hallucinogen.
+ **/
+/obj/item/organ/internal/liver/bone/plasmaman
+	name = "reagent processing crystal"
+	desc = "A large crystal that is somehow capable of metabolizing chemicals, these are found in plasmamen."
+	icon_state = "liver-p"
+	status = ORGAN_MINERAL
+
+/obj/item/organ/internal/liver/bone/plasmaman/handle_chemical(mob/living/carbon/organ_owner, datum/reagent/chem, seconds_per_tick, times_fired)
+	. = ..()
+	//parent returned COMSIG_MOB_STOP_REAGENT_CHECK or we are failing
+	if(. || (organ_flags & ORGAN_FAILING))
+		return
+	// plasmamen use plasma to reform their bones or whatever
+	if(istype(chem, /datum/reagent/toxin/plasma) || istype(chem, /datum/reagent/toxin/hot_ice))
+		for(var/datum/wound/iter_wound as anything in organ_owner.all_wounds)
+			iter_wound.on_xadone(4 * REM * seconds_per_tick)
+		return // Do normal metabolism
+	if(istype(chem, /datum/reagent/gunpowder))
+		organ_owner.set_timed_status_effect(15 SECONDS * seconds_per_tick, /datum/status_effect/drugginess)
+		if(organ_owner.get_timed_status_effect_duration(/datum/status_effect/hallucination) / 10 < chem.volume)
+			organ_owner.adjust_hallucinations(2.5 SECONDS * seconds_per_tick)
+		return // Do normal metabolism

emtee40/Monkestation2

code/modules/surgery/organs/internal/liver/liver_plasmaman.dm
@@ -1,0 +1,25 @@
+/**
+ * Plasmaman liver
+ * Makes plasma and hot ice heal wounds, also makes gunpowder a hallucinogen.
+ **/
+/obj/item/organ/internal/liver/bone/plasmaman
+	name = "reagent processing crystal"
+	desc = "A large crystal that is somehow capable of metabolizing chemicals, these are found in plasmamen."
+	icon_state = "liver-p"
+	organ_flags = ORGAN_MINERAL
+
+/obj/item/organ/internal/liver/bone/plasmaman/handle_chemical(mob/living/carbon/organ_owner, datum/reagent/chem, seconds_per_tick, times_fired)
+	. = ..()
+	//parent returned COMSIG_MOB_STOP_REAGENT_CHECK or we are failing
+	if(. || (organ_flags & ORGAN_FAILING))
+		return
+	// plasmamen use plasma to reform their bones or whatever
+	if(istype(chem, /datum/reagent/toxin/plasma) || istype(chem, /datum/reagent/toxin/hot_ice))
+		for(var/datum/wound/iter_wound as anything in organ_owner.all_wounds)
+			iter_wound.on_xadone(4 * REM * seconds_per_tick)
+		return // Do normal metabolism
+	if(istype(chem, /datum/reagent/gunpowder))
+		organ_owner.set_timed_status_effect(15 SECONDS * seconds_per_tick, /datum/status_effect/drugginess)
+		if(organ_owner.get_timed_status_effect_duration(/datum/status_effect/hallucination) / 10 < chem.volume)
+			organ_owner.adjust_hallucinations(2.5 SECONDS * seconds_per_tick)
+		return // Do normal metabolism