HolyTigerGod/Voidcrew

Cleans up + Improves bows, Sorts files, Adds the Divine Archer clothing, weapon, rite (#74811)
## About The Pull Request

### Divine Archer 🏹 


![image](https://user-images.githubusercontent.com/40974010/232647927-aace69ea-bda8-4ec9-9bf1-60140034fbb3.png)

Adds a new chaplain weapon and suit of armor, the divine archer. It's an
orderable set of armor, but provides less armor than the rest, but you
get more pieces of armor (boots, bracer, undersuit).

The divine bow comes with a quiver that holds holy arrows. The holy
arrows come with bane support, dealing critical damage to revenants.

### Bow Features ⭐ 

- arrows can now be dipped in poison

### Bow Improvements 🔧 

- bows now drop their arrow when you put them on your back while nocking
a bow
- bows give feedback for trying to draw without a nocked arrow
- codewise, bows support subtypes much better. They still have
hard-sprited loaded arrows, but one day that'll change.

## Why It's Good For The Game

Yeah, we could add null rod #2342 that does almost the same as the
others, or we could have a unique bow weapon!

Player Dev Project thread:
https://discord.com/channels/326822144233439242/1093521091957370940/1093521091957370940

## Changelog
:cl: tralezab code, Drag for the commission and player project, cre#0484
for their spritework
add: Divine Archer Armor and Weapon
qol: Bows give more feedback when you're doing something wrong, like
trying to draw without a nocked arrow
code: Sorted files, cleaned bow code up to allow subtypes
/:cl:

code/modules/projectiles/guns/ballistic/bows/bow_types.dm
@@ -1,0 +1,47 @@
+
+///basic bow, used for medieval sim
+/obj/item/gun/ballistic/bow/longbow
+	name = "longbow"
+	desc = "While pretty finely crafted, surely you can find something better to use in the current year."
+
+///chaplain's divine archer bow
+/obj/item/gun/ballistic/bow/divine
+	name = "divine bow"
+	desc = "Holy armament to pierce the souls of sinners."
+	icon_state = "holybow"
+	inhand_icon_state = "holybow"
+	base_icon_state = "holybow"
+	worn_icon_state = "holybow"
+	slot_flags = ITEM_SLOT_BACK
+	mag_type = /obj/item/ammo_box/magazine/internal/bow/holy
+
+/obj/item/ammo_box/magazine/internal/bow/holy
+	name = "divine bowstring"
+	ammo_type = /obj/item/ammo_casing/caseless/arrow/holy
+
+/obj/item/gun/ballistic/bow/divine/Initialize(mapload)
+	. = ..()
+	AddComponent(/datum/component/anti_magic, MAGIC_RESISTANCE|MAGIC_RESISTANCE_HOLY)
+	AddComponent(/datum/component/effect_remover, \
+		success_feedback = "You disrupt the magic of %THEEFFECT with %THEWEAPON.", \
+		success_forcesay = "BOW-GONE FOUL MAGIKS!!", \
+		tip_text = "Clear rune", \
+		on_clear_callback = CALLBACK(src, PROC_REF(on_cult_rune_removed)), \
+		effects_we_clear = list(/obj/effect/rune, /obj/effect/heretic_rune) \
+	)
+	AddElement(/datum/element/bane, target_type = /mob/living/simple_animal/revenant, damage_multiplier = 0, added_damage = 25, requires_combat_mode = FALSE)
+
+/obj/item/gun/ballistic/bow/divine/proc/on_cult_rune_removed(obj/effect/target, mob/living/user)
+	SIGNAL_HANDLER
+	if(!istype(target, /obj/effect/rune))
+		return
+
+	var/obj/effect/rune/target_rune = target
+	if(target_rune.log_when_erased)
+		user.log_message("erased [target_rune.cultist_name] rune using a null rod", LOG_GAME)
+		message_admins("[ADMIN_LOOKUPFLW(user)] erased a [target_rune.cultist_name] rune with a null rod.")
+	SSshuttle.shuttle_purchase_requirements_met[SHUTTLE_UNLOCK_NARNAR] = TRUE
+
+/obj/item/gun/ballistic/bow/divine/with_quiver/Initialize(mapload)
+	. = ..()
+	new /obj/item/storage/bag/quiver/holy(loc)

emtee40/Monkestation2

code/modules/projectiles/guns/ballistic/bows/bow_types.dm
@@ -1,0 +1,47 @@
+
+///basic bow, used for medieval sim
+/obj/item/gun/ballistic/bow/longbow
+	name = "longbow"
+	desc = "While pretty finely crafted, surely you can find something better to use in the current year."
+
+///chaplain's divine archer bow
+/obj/item/gun/ballistic/bow/divine
+	name = "divine bow"
+	desc = "Holy armament to pierce the souls of sinners."
+	icon_state = "holybow"
+	inhand_icon_state = "holybow"
+	base_icon_state = "holybow"
+	worn_icon_state = "holybow"
+	slot_flags = ITEM_SLOT_BACK
+	mag_type = /obj/item/ammo_box/magazine/internal/bow/holy
+
+/obj/item/ammo_box/magazine/internal/bow/holy
+	name = "divine bowstring"
+	ammo_type = /obj/item/ammo_casing/caseless/arrow/holy
+
+/obj/item/gun/ballistic/bow/divine/Initialize(mapload)
+	. = ..()
+	AddComponent(/datum/component/anti_magic, MAGIC_RESISTANCE|MAGIC_RESISTANCE_HOLY)
+	AddComponent(/datum/component/effect_remover, \
+		success_feedback = "You disrupt the magic of %THEEFFECT with %THEWEAPON.", \
+		success_forcesay = "BOW-GONE FOUL MAGIKS!!", \
+		tip_text = "Clear rune", \
+		on_clear_callback = CALLBACK(src, PROC_REF(on_cult_rune_removed)), \
+		effects_we_clear = list(/obj/effect/rune, /obj/effect/heretic_rune) \
+	)
+	AddElement(/datum/element/bane, target_type = /mob/living/basic/revenant, damage_multiplier = 0, added_damage = 25, requires_combat_mode = FALSE)
+
+/obj/item/gun/ballistic/bow/divine/proc/on_cult_rune_removed(obj/effect/target, mob/living/user)
+	SIGNAL_HANDLER
+	if(!istype(target, /obj/effect/rune))
+		return
+
+	var/obj/effect/rune/target_rune = target
+	if(target_rune.log_when_erased)
+		user.log_message("erased [target_rune.cultist_name] rune using a null rod", LOG_GAME)
+		message_admins("[ADMIN_LOOKUPFLW(user)] erased a [target_rune.cultist_name] rune with a null rod.")
+	SSshuttle.shuttle_purchase_requirements_met[SHUTTLE_UNLOCK_NARNAR] = TRUE
+
+/obj/item/gun/ballistic/bow/divine/with_quiver/Initialize(mapload)
+	. = ..()
+	new /obj/item/storage/bag/quiver/holy(loc)