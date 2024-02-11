f0b6ea7a4da5ec4b1edb08f65e828da789813047
Gboster-0 <82319946+Gboster-0@users.noreply.github.com>
2024-02-11 08:59:28 +0100

    bit more TGUI work

--- tgui/packages/tgui/interfaces/AntagInfoBorer.tsx
+++ tgui/packages/tgui/interfaces/AntagInfoBorer.tsx
@@ -67,9 +67,25 @@ export const AntagInfoBorer = (props: any, context: any) => {
             onClick={() => setTab(2)}>
             Ability explanations
           </Tabs.Tab>
+          <Tabs.Tab
+            icon="list"
+            lineHeight="23px"
+            selected={tab === 3}
+            onClick={() => setTab(3)}>
+            Borer side-effects
+          </Tabs.Tab>
+          <Tabs.Tab
+            icon="list"
+            lineHeight="23px"
+            selected={tab === 4}
+            onClick={() => setTab(4)}>
+            Basic chemical information
+          </Tabs.Tab>
         </Tabs>
         {tab === 1 && <MainPage />}
         {tab === 2 && <BorerAbilities />}
+        {tab === 3 && <DisadvantageInfo />}
+        {tab === 4 && <BasicChemistry />}
       </Window.Content>
     </Window>
   );
@@ -156,7 +172,7 @@ const BorerAbilities = (props: any, context: any) => {
   const { act, data } = useBackend<BorerInformation>(context);
   return (
     <Stack vertical fill>
-      <Stack.Item minHeight="20rem">
+      <Stack.Item minHeight="42rem">
         <AbilitySection />
       </Stack.Item>
     </Stack>
@@ -220,5 +236,168 @@ const AbilitySection = (props: any, context: any) => {
         </Stack.Item>
       </Stack>
     </Section>
+  );
+};
+
+const DisadvantageInfo = () => {
+  return (
+    <Stack vertical fill>
+      <Stack.Item minHeight="42rem">
+        <Section fill title="How i didnt kill my host 101">
+          <Stack vertical>
+            <Stack.Item>
+              <span>
+                Whilst in a host you can provide many benefits, but also
+                dangerous side-effects due to your sensitive brain manipulation.
+                Here&apos;s how to prevent them
+              </span>
+              <br />
+              <br />
+              <span>
+                1. Whilst inside of a host we will passivelly make their health
+                unable to be read due to our body obstructing the somatosensory
+                cortex signals
+              </span>
+              <br />
+              <span>
+                Prevention method - observe the hosts health carefully using
+                &quot;Check Blood&quot;, heal any injuries and inform the host
+                about any major wounds
+              </span>
+              <br />
+              <br />
+              <span>
+                2. Whilst inside of a host we will slowly deal toxin damage
+                over-time up to 80 in total. This can be deadly when combined
+                with any amount of brute/burn damage
+              </span>
+              <br />
+              <span>
+                Prevention method - observe the hosts health carefully using
+                &quot;Check Blood&quot;, inject toxin damage restoring chemicals
+              </span>
+              <br />
+              <br />
+              <span>
+                3. Whilst inside of a host most of our actions will deal brain
+                damage including generating evolution and chemical evolution
+                points, due to eighter sensetivelly manipulating the host&apos;s
+                neurons or needing to &quot;aquire&quot; more space for growth
+              </span>
+              <br />
+              <span>
+                Prevention method - observe the hosts health carefully using
+                &quot;Check Blood&quot;, inject mannitol to cure brain damage,
+                inject neurine for any brain traumas that might have been a
+                result of our expansion
+              </span>
+            </Stack.Item>
+          </Stack>
+        </Section>
+      </Stack.Item>
+    </Stack>
+  );
+};
+
+const BasicChemistry = () => {
+  return (
+    <Stack vertical fill>
+      <Stack.Item minHeight="45rem">
+        <Section fill title="What does the eldritch essence do again?">
+          <Stack vertical>
+            <Stack.Item>
+              <span>
+                Secreting chemicals has proven difficult for many borers, yet
+                you have prepared carefully for your first expendition into the
+                hosts body. Lets not mess it up by killing them.
+              </span>
+              <br />
+              <span>
+                This is only the bare minimum of what we should get knowledgable
+                about
+              </span>
+              <br />
+              <br />
+              <span>Unknown Methamphetamine Isomer</span>
+              <br />
+              <span>
+                A specially advanced version of what our hosts call
+                &quot;meth&quot;. It has all the benefits of meth without
+                causing any brain damage to the host and has a higher overdose
+              </span>
+              <br />
+              <span>Overdose: 40 units</span>
+              <br />
+              <br />
+              <span>Spaceacillin</span>
+              <br />
+              <span>
+                Helps our hosts immune system, making it quickly gain resistance
+                to any pathogens inside of the host.
+              </span>
+              <br />
+              <span>
+                While being effective it will most likelly not be enough to
+                fully cure our host
+              </span>
+              <br />
+              <br />
+              <span>Convermol</span>
+              <br />
+              <span>
+                Quickly restores our hosts
+                <span className={'color-blue'}> Oxygen </span>damage at the cost
+                of causing 1:5th the toxin damage to our host
+              </span>
+              <br />
+              <span>Overdose: 35 units</span>
+              <br />
+              <br />
+              <span>Lenturi</span>
+              <br />
+              <span>
+                Quickly restores our hosts
+                <span className={'color-yellow'}> Burn </span>damage at the cost
+                of causing slight stomach damage and slowing down our host as
+                long as its in their system
+              </span>
+              <br />
+              <br />
+              <span>Libital</span>
+              <br />
+              <span>
+                Quickly restores our hosts
+                <span className={'color-red'}> Brute </span>damage at the cost
+                of causing slight liver damage.
+              </span>
+              <br />
+              <br />
+              <span>multiver</span>
+              <br />
+              <span>
+                Purges toxins and medicines inside of our host while healing
+                <span className={'color-green'}> Toxin </span>damage, at the
+                cost of slight lung damage.
+              </span>
+              <br />
+              <span>
+                The more unique medicines the host has in their system, the more
+                this chemical heals.
+              </span>
+              <br />
+              <span>At 2 unique medicines it no longer purges medicines</span>
+              <br />
+              <br />
+              <span>Seiver</span>
+              <br />
+              <span>
+                Heals<span className={'color-green'}> Toxin </span>damage at the
+                slight cost of heart damage
+              </span>
+            </Stack.Item>
+          </Stack>
+        </Section>
+      </Stack.Item>
+    </Stack>
   );
 };
