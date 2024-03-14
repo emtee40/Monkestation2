5545b5bf2b73fcebb67215f06f13cf4b641ad28a
Lucy <lucy@absolucy.moe>
2024-03-13 20:52:20 -0400

    Of COURSE slashers never had any role prefs lmao

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/slasher.ts
@@ -0,0 +1,11 @@
+import { Antagonist, Category } from '../base';
+// import { multiline } from 'common/string';
+
+const Slasher: Antagonist = {
+  key: 'slasher',
+  name: 'Slasher',
+  description: ['TODO: get someone better at writing than me to describe this'],
+  category: Category.Midround,
+};
+
+export default Slasher;
