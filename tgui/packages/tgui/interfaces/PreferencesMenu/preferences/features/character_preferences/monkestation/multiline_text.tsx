1e1304c899023747c177fee935f2b3f44b45fd62
Hardly3D <66234359+Hardly3D@users.noreply.github.com>
2024-05-07 23:31:35 -0300

    Damage control

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/monkestation/multiline_text.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/monkestation/multiline_text.tsx
@@ -1,54 +1,79 @@
-import { Feature, FeatureValueProps } from '../../base';
-import { Stack, TextArea } from '../../../../../../components';
+import { Box, TextArea } from '../../../../../../components';
+import { Feature, FeatureShortTextData, FeatureValueProps } from '../../base';
+
+export type FeatureMultiline = Feature<string, string, FeatureShortTextData>;
+export type FeatureMultilineProps = FeatureValueProps<
+  string,
+  string,
+  FeatureShortTextData
+>;
 
 export const MultilineText = (
-  props: FeatureValueProps<string, string> & { box_height: string | null }
+  props: FeatureMultilineProps & {
+    box_height: string | null;
+  }
 ) => {
-  const { handleSetValue, value } = props;
+  if (!props.serverData) {
+    return <Box>Loading...</Box>;
+  }
   return (
-    <Stack>
-      <Stack.Item grow>
-        <TextArea
-          width="80%"
-          height={props.box_height || '36px'}
-          value={value}
-          onInput={(e, value) => {
-            handleSetValue(value);
-          }}
-        />
-      </Stack.Item>
-    </Stack>
+    <TextArea
+      width="80%"
+      height={props.box_height || '36px'}
+      value={props.value}
+      maxLength={props.serverData.maximum_length || 1024}
+      onChange={(_, new_value) => {
+        props.handleSetValue(new_value);
+      }}
+    />
   );
 };
 
-export const flavor_text: Feature<string, string> = {
+export const flavor_text: FeatureMultiline = {
   name: 'Flavor - Flavor Text',
-  component: (props: FeatureValueProps<string, string>, context) => {
+  description:
+    'A small snippet of text shown when others examine you, \
+    describing what you may look like.',
+  component: (props: FeatureMultilineProps) => {
     return <MultilineText {...props} box_height="52px" />;
   },
 };
 
-export const silicon_text: Feature<string, string> = {
+export const silicon_text: FeatureMultiline = {
   name: 'Flavor - Silicon Flavor Text',
+  description: 'Flavor text shown when you are placed into a cyborg or AI.',
   component: MultilineText,
 };
 
-export const exploitable_info: Feature<string, string> = {
+export const exploitable_info: FeatureMultiline = {
   name: 'Flavor - Exploitable Info',
+  description:
+    'Information about your character made available to \
+    players who are antagonists. Can be used to give antagonists \
+    more interesting ways of approaching your character.',
   component: MultilineText,
 };
 
-export const general_records: Feature<string, string> = {
+export const general_records: FeatureMultiline = {
   name: 'Flavor - General Records',
+  description:
+    "Random information about your character's history. \
+    Available in medical records consoles.",
   component: MultilineText,
 };
 
-export const security_records: Feature<string, string> = {
+export const security_records: FeatureMultiline = {
   name: 'Flavor - Security Records',
+  description:
+    "Information about your character's criminal past. \
+    Available in security records consoles.",
   component: MultilineText,
 };
 
-export const medical_records: Feature<string, string> = {
+export const medical_records: FeatureMultiline = {
   name: 'Flavor - Medical Records',
+  description:
+    "Information about your character's medical history. \
+    Available in medical records consoles.",
   component: MultilineText,
 };
